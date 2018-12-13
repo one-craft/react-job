446a63e124bb2f95f56d42166ace8dbc8a722160
张颀 <excit1ng@126.com>
2018-12-14 00:00:47 +0800

    fixed bugs

--- src/components/Boss/index.js
+++ src/components/Boss/index.js
@@ -1,6 +1,6 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, Fragment } from 'react';
 import Axios from 'axios';
-import { Card, WingBlank } from 'antd-mobile';
+import { Card, WingBlank, WhiteSpace } from 'antd-mobile';
 
 export default class Boss extends PureComponent {
   state = {
@@ -8,7 +8,7 @@ export default class Boss extends PureComponent {
   }
 
   componentDidMount() {
-    Axios.get('user/list?type=genius')
+    Axios.get('user/list?type=boss')
       .then(res => {
         if(res.data.code === 0) {
           this.setState({data: res.data.data})
@@ -25,13 +25,16 @@ export default class Boss extends PureComponent {
               return null;
             }
             return (
-              <Card>
-                <Card.Header 
-                  title={user}
-                  thumb={require(`../../asset/${avatar}.png`)}
-                  extra={<span>{title}</span>}
-                />
-              </Card>
+              <Fragment>
+                <WhiteSpace />
+                <Card>
+                  <Card.Header 
+                    title={user}
+                    thumb={require(`../../asset/${avatar}.png`)}
+                    extra={<span>{title}</span>}
+                  />
+                </Card>
+              </Fragment>
             )
           })
         }
